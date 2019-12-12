72634d183a92a45844b4eea40f1e24d9a017777b
ovidiu-petridean <petridean.ovidiu@yahoo.com>
2019-12-12 20:07:38 +0200

    Update ConvertTemperature.java

--- ConvertTemperature.java
+++ ConvertTemperature.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class ConvertTemperature {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+//Change this to be System.out.....
         System.out.print("Input a degree in Fahrenheit: ");
         double fahrenheit = input.nextDouble();
 
